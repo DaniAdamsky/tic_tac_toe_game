d8bce5a054045519b423b9a6228957a704e91262
DaniA <daniad01@icloud.com>
2026-03-15 17:01:07 +0200

    bug fixes and repairs

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -4,7 +4,7 @@ player1 = random.choice(['✖️', '⭕'])
 player2 = '✖️' if player1 == '⭕' else '⭕'
 
 
-def main_menu():
+def main_menu() -> int:
     """menu choice for who to play against
     1 == against cpu
     2 == 2 player"""
@@ -26,7 +26,7 @@ def main_menu():
 menu_choice = main_menu()
 
 
-def create_board():
+def create_board() -> list:
     """returns a clear board list
     with 1-9"""
     return [str(i + 1) for i in range(9)]
@@ -45,16 +45,17 @@ def get_move(current_player, board):
     function checks whether the move is valid"""
     while True:
         if menu_choice == 1 and player2 == current_player:
-            """checks if current player is the cpu"""
+            """checks if menu choice was play against cpu and also if it is the cpu's turn, if true - cpu makes the move"""
             # cpu move
-            player_move = cpu_move()
+            player_move = cpu_move(board)
         else:
             # player move
-            player_move = input(f"Player {current_player}, Please enter your move:\n(to restart mid-game, input -999 as a move)\n")
+            player_move = input(
+                f"Player {current_player}, Please enter your move:\n(to restart mid-game, input -999 as a move)\n")
         if player_move == "-999":
             # Easy bonus: add a restart option mid-game
             print("restarting game...")
-            play_game()
+            return play_game()
         elif not player_move.isdigit():
             print("Please enter a valid move")
         elif int(player_move) not in range(1, 10):
@@ -66,18 +67,18 @@ def get_move(current_player, board):
 
 
 def make_move(board, position, symbol):
-    """makes a move on the board for the chosen player"""
-    board[int(position) - 1] = symbol
+    """makes a move on the board for the current player"""
+    board[position - 1] = symbol
 
 
 def check_winner(board, symbol) -> bool:
     """returns True if a player won the game (3 in a row/column/diagonal line)"""
-    combos = [
+    winning_combos = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8],
         [0, 3, 6], [1, 4, 7], [2, 5, 8],
         [0, 4, 8], [2, 4, 6]
     ]
-    for combo in combos:
+    for combo in winning_combos:
         # check all possible combos for win
         if all(board[i] == symbol for i in combo):
             return True
@@ -85,7 +86,7 @@ def check_winner(board, symbol) -> bool:
 
 
 def is_tie(board) -> bool:
-    """return true if there are no more valid moves"""
+    """check all squares and return true if there are no more valid moves"""
     for square in board:
         # checks if board is full
         if square != '✖️' and square != '⭕':
@@ -93,7 +94,7 @@ def is_tie(board) -> bool:
     return True
 
 
-def switch_player(current):
+def switch_player(current) -> str:
     """switches the current player from one to the other"""
     if current == '✖️':
         return '⭕'
@@ -103,6 +104,7 @@ def switch_player(current):
 
 # bonus function
 def score_board(board, player, ties, player_x_score, player_o_score) -> tuple:
+    """score board indexer and announcer"""
     if is_tie(board):
         ties += 1
     if check_winner(board, player):
@@ -115,7 +117,7 @@ def score_board(board, player, ties, player_x_score, player_o_score) -> tuple:
 
 
 # Bonus function - computer player
-def cpu_move() -> str:
+def cpu_move(board) -> str:
     """Cpu player moves"""
     return str(random.randint(1, 9))
 
@@ -139,10 +141,12 @@ def play_game(tie_amount=0, x_score=0, o_score=0):
         else:
             current_player = switch_player(current_player)
     tie_amount, x_score, o_score = score_board(board, current_player, tie_amount, x_score, o_score)
-    play_again = input("Do you want to play again? (Y/N): ")
-    if play_again == 'Y':
+    play_again = input("Do you want to play again? (y/n): ")
+    if play_again == 'y':
         print("restarting game...")
         play_game(tie_amount, x_score, o_score)
+    else:
+        print("goodbye! thanks for playing")
 
 
 play_game()
